d4ed80206411a634980c80a8eb78e65305675455
NgoPhucThinhcnttk19 <vnjames8@gmail.com>
2024-03-29 16:05:52 +0700

    first commit

---
+++ WebApplication9/Models/Genre.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication9.Models
+{
+    public enum Genre
+        {
+            Fantasy,
+            Adventure,
+            Romance,
+            Contemporary,
+            Dystopian,
+            Mystery,
+            Horror,
+            Thriller,
+            Paranormal,
+            Historical_fiction,
+            Science_Fiction,
+            Childrens,
+            Memoir,
+            Cooking,
+            Art,
+            Self_help,
+            Personal,
+            Development,
+            Motivational,
+            Health,
+            History,
+            Travel,
+            How_to,
+            Families_Relationships,
+            Humor,
+            Biography
+        }
+    }
